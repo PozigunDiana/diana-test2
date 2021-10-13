e38bc97db1ccee887426fab4955b6c2ccfba5e66
Roman <nokerka@ya.ru>
2021-10-13 11:17:24 +0300

    Revert "Add Comments"
    This reverts commit 363bc3b0c5ae1259312cada7fa3d0e84270800b0.

--- info.php
+++ info.php
@@ -2,8 +2,6 @@
 
 #Git
 # git status
-# git pull //перед началом работы не забываем «накатить» в локальный репозиторий последнюю версию кода
-# git checkout master // переключаемся на master
 # git add info.php
 # git commit -m "Add Comments"
 # git push
