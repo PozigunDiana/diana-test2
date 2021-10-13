164880044f9a4bec6ac10e797b0bf607637c2651
Roman <nokerka@ya.ru>
2021-10-13 11:20:19 +0300

    Add Comments

--- info.php
+++ info.php
@@ -2,7 +2,8 @@
 
 #Git
 # git status
+# git pull //перед началом работы не забываем «накатить» в локальный репозиторий последнюю версию кода
+# git checkout master // переключаемся на master
 # git add info.php
 # git commit -m "Add Comments"
 # git push
-
